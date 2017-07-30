ee098290e2ece78249b52481ad77a1cb8081bfba
XingMeansOK <771267736@qq.com>
2017-07-30 19:42:54 +0800

    load stations

--- src/components/inputpage.js
+++ src/components/inputpage.js
@@ -2,7 +2,8 @@ import {
   Text,
   View,
   Button,
-  TextInput
+  TextInput,
+  ListView
 } from 'react-native';
 import React, {Component} from 'react';
 import { RESULTPAGE } from './constant'; // 导入常量，避免耦合
@@ -11,16 +12,63 @@ import {observer} from 'mobx-react';
 
 @observer
 export default class Inputpage extends Component {
+  constructor(props) {
+    super(props);
+    this.store = props.screenProps;// 将appStore的引用作为当前对象的成员保存下来，方便使用
+    this.state = {
+      isLoading: true
+    }
+
+    this.listComponent = () => {
+      if(this.state.isLoading) {
+        return (<Text>loading</Text>)
+      } else {
+        return (
+          <ListView
+            dataSource={this.store.stations}
+            renderRow={(rowData) => <Text>{rowData.pinyin}, {rowData.name}</Text>}
+          />
+        )
+      }
+    }
+
+  }
   static navigationOptions = {
     title: 'type in',
   };
+
+  componentDidMount() {//组件加载之后，向服务器请求火车站站名信息
+    return fetch('http://192.168.0.126:3000/json/name.json')
+      .then((response) => {
+        return response.json()
+      })
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        // this.setState({
+        //   isLoading: false,
+        //   dataSource: ds.cloneWithRows(responseJson.movies),
+        // }, function() {
+        //   // do something with new state
+        // });
+        this.store.stations = ds.cloneWithRows(responseJson.trainData);
+        this.setState({isLoading: false});
+
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+
+
   render() {
     const { navigate } = this.props.navigation;
-    const store = this.props.screenProps;
+    const store = this.store;
     return (
       <View>
         <TextInput onChangeText={(text) => store.start = text}/>
         <Text>{store.start}</Text>
+        {this.listComponent()}
       </View>
     );
   }
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import {observable, computed, reaction} from 'mobx';
 
 class AppStore {
-  @observable start = '出发站';
-  @observable destination = '终到站';
+  @observable start = '出发站'; // 用于存放用户输入的始发站
+  @observable destination = '终到站'; // 用于存放用户输入的终点站
+  @observable stations = [];
 }
 
 export const appStore = new AppStore();
